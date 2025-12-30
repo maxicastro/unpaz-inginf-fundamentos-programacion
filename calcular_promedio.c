50c38f03d52b7ae3c6b3f6f386371209b022a174
Maxi Castro <maxicastrosistemas@gmail.com>
2025-12-30 20:11:30 -0300

    ej calcular_promedio en la intro de arrays

---
+++ calcular_promedio.c
@@ -0,0 +1,9 @@
+#include "lib/utilidades.h"
+
+int main(void) {
+    float puntaje_1 = 73.0;
+    float puntaje_2 = 70.0;
+    float puntaje_3 = 5.40;
+
+    printf("Promedio: %.2f.\n", (puntaje_1 + puntaje_2 + puntaje_3) / 3);
+}
