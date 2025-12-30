23927b5860529357f752d79460291ec82c29e772
Maxi Castro <maxicastrosistemas@gmail.com>
2025-12-30 18:04:08 -0300

    ejemplos buggy

---
+++ buggy_2.c
@@ -0,0 +1,7 @@
+#include "lib/utilidades.h"
+
+int main(void)
+{
+    nombre = leer_string("Cómo es tu nombre?");
+    printf("Hola %s!\n", nombre);
+}
---
+++ buggy_3.c
@@ -0,0 +1,6 @@
+#include "lib/utilidades.h"
+
+int main(void)
+{
+    int edad = leer_int("Cuál es tu edad?");
+}
