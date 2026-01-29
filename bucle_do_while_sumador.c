8bc70fa21c6bd1b99c0d1330b990f82f8f0bea1e
Maxi Castro <maxicastrosistemas@gmail.com>
2026-01-29 19:30:59 -0300

    ejemplo bucle_do_while_sumador.c

---
+++ bucle_do_while_sumador.c
@@ -0,0 +1,20 @@
+#include "lib/utilidades.h"
+
+int main(void)
+{
+    int numero;
+    int total = 0;
+    char continuar;
+
+    do {
+        numero = leer_int("Ingresá un número para sumar: ");
+        total += numero;
+        printf("Total parcial: %d\n", total);
+
+        continuar = leer_char("¿Querés sumar otro número?': ");
+
+    } while (continuar == 's' || continuar == 'S');
+
+    printf("Resultado final de la suma: %d\n", total);
+    return 0;
+}
